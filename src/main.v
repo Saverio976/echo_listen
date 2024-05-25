9d27bad3e2ff485bd8688132765048d03f5b490c
Xavier Mitault <xavier.mitault@epitech.eu>
2024-05-25 13:51:47 +0200

    Add some sentence

--- src/main.v
+++ src/main.v
@@ -12,43 +12,31 @@ pub struct App {
 
 @[get; post]
 pub fn (app &App) history(mut ctx Context) veb.Result {
-	echo := 'STARTDELIM ------------------------------------------ ENDDELIM\n'
-		+ '-------------------- ${ctx.ip()} ----------------------\n'
-		+ '-------------------- header\n'
-		+ '${ctx.req.header.str().trim("\n")}\n'
-		+ '-------------------- data\n'
-		+ '${ctx.req.data.trim("\n")}\n'
-		+ '------------------------------------------\n'
-	file_out := os.getenv_opt('ECHOLISTEN_HISTORY_ROUTE') or {
-		'/tmp/echolisten_history_route'
-	}
-	mut f := os.open_append(file_out) or {
-		return ctx.text(echo + 'ERR')
-	}
+	echo := 'STARTDELIM ------------------------------------------ ENDDELIM\n' +
+		'-------------------- ${ctx.ip()} ----------------------\n' +
+		'-------------------- header\n' + '${ctx.req.header.str().trim('\n')}\n' +
+		'-------------------- data\n' + '${ctx.req.data.trim('\n')}\n' +
+		'------------------------------------------\n'
+	file_out := os.getenv_opt('ECHOLISTEN_HISTORY_ROUTE') or { '/tmp/echolisten_history_route' }
+	mut f := os.open_append(file_out) or { return ctx.text(echo + 'ERR') }
 	defer {
 		f.close()
 	}
-	f.writeln(echo) or {
-		return ctx.text(echo + 'ERR')
-	}
+	f.writeln(echo) or { return ctx.text(echo + 'ERR') }
 	println('+1 ${ctx.ip()}')
 	return ctx.text(echo)
 }
 
 @[get; post]
 pub fn (app &App) history_result(mut ctx Context) veb.Result {
-	file_out := os.getenv_opt('ECHOLISTEN_HISTORY_ROUTE') or {
-		'/tmp/echolisten_history_route'
-	}
-	echo := os.read_file(file_out) or {
-		return ctx.text('ERR')
-	}
+	file_out := os.getenv_opt('ECHOLISTEN_HISTORY_ROUTE') or { '/tmp/echolisten_history_route' }
+	echo := os.read_file(file_out) or { return ctx.text('ERR') }
 	return ctx.text(echo)
 }
 
 @[get; post]
 pub fn (app &App) index(mut ctx Context) veb.Result {
-	return ctx.text("")
+	return ctx.text("If it is the author that curl'd it on your machine, it is intended as a joke. If it is someone else, the author will not sell/use the data given. Sorry.")
 }
 
 fn main() {
